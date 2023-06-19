9e2572fd3b56250c9b6254cad2579e42db4b7912
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2023-06-19 22:35:53 +0200

    Drop duplicates, sort results

--- src/pocs_xml2csv.py
+++ src/pocs_xml2csv.py
@@ -13,6 +13,19 @@ __author__ = "Jean Pommier"
 __license__ = "Apache"
 __email__ = "jean.pommier@pi-geosolutions.fr"
 
+csv_delimiter=';'
+
+
+def drop_empty_string(list_of_strings, inline=True):
+    """
+    Drop strings that are empty or just containing spaces from the given list of strings
+    :return:
+    """
+    new_list = [x for x in list_of_strings if x.strip() != '']
+    if inline:
+        list_of_strings = new_list
+    return new_list
+
 
 @click.command()
 @click.argument('source_file')
@@ -26,7 +39,8 @@ def pocs_xml2csv(source_file,
     :param output_file:
     :return:
     """
-    pocs_list = []
+    pocs_list = set()
+    pocs_fields = ("org_name", "role", "email", "ind_name", "ind_position")
 
     # Scan the xml file and extract the list of valid POCs
     try:
@@ -36,6 +50,7 @@ def pocs_xml2csv(source_file,
                               'gco': 'http://standards.iso.org/iso/19115/-3/gco/1.0',
                           }
         pocs = tree.xpath('.//cit:CI_Responsibility', namespaces=cit_namespaces )
+        print(f'Reading {len(pocs)} entries from XML file')
         for poc in pocs:
             org_name = poc.xpath('.//cit:party/cit:CI_Organisation/cit:name/*/text()', namespaces=cit_namespaces)
             role = poc.xpath('.//cit:role/cit:CI_RoleCode/@codeListValue', namespaces=cit_namespaces)
@@ -47,16 +62,18 @@ def pocs_xml2csv(source_file,
                 print(f"WARNING: found logo information in record {org_name},{email},{ind_name},{ind_position}. We don't support logos yet")
             # print(f'{org_name},{email}')
             if email:
-                pocs_list.append({
-                    "org_name": ",".join(org_name),
-                    "role": ",".join(role),
-                    "email": ",".join(email),
-                    "ind_name": ",".join(ind_name),
-                    "ind_position": ",".join(ind_position),
-                })
+
+                pocs_list.add((",".join(drop_empty_string(org_name)),
+                    ",".join(drop_empty_string(role)),
+                    ",".join(drop_empty_string(email)),
+                    ",".join(drop_empty_string(ind_name)),
+                    ",".join(drop_empty_string(ind_position)),
+                                    ))
     except etree.ParseError as e:
         print(f"Oops, cannot parse {source_file}")
         print(e)
+
+    print(f'Got {len(pocs_list)} distinct results')
 
     if not pocs_list:
         print(f"List of parsed pocs is empty, there might be an issue")
@@ -64,13 +81,13 @@ def pocs_xml2csv(source_file,
 
     # Write the results
     if output_file:
-        csv_columns = pocs_list[0].keys()
+        csv_columns = pocs_fields
         csv_file = output_file
         try:
             with open(csv_file, 'w') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-                writer.writeheader()
-                for data in pocs_list:
+                writer = csv.writer(csvfile, delimiter=csv_delimiter, quotechar='"')
+                writer.writerow(csv_columns)
+                for data in sorted(pocs_list):
                     writer.writerow(data)
         except IOError:
             print("I/O error")
@@ -79,9 +96,9 @@ def pocs_xml2csv(source_file,
         print('    ----------------------------------------      ')
         print('You did not set an output file. Writing to stdout:')
         print('    ----------------------------------------      ')
-        print(",".join(pocs_list[0].keys()))
+        print(csv_delimiter.join(pocs_fields))
         for p in pocs_list:
-            print(",".join(p.values()))
+            print(csv_delimiter.join(p))
 
 
 if __name__ == '__main__':
